a66efd36685d2db40719b5287f06c867de93db15
pepoangel <angeldavid@gmail.com>
2023-06-11 10:59:45 -0400

    volviendo a t2micro

--- main.tf
+++ main.tf
@@ -51,7 +51,7 @@ resource "aws_security_group" "allow_ssh" {
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
+  instance_type = "t2.micro"
   key_name      = aws_key_pair.deployer.key_name
   vpc_security_group_ids = [
     aws_security_group.allow_ssh.id
