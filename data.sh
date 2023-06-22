89e30e4b802717391328d2ad7cb141d801f91f51
Pepo <angeldavid@gmail.com>
2023-06-22 19:39:14 -0400

    agregando

--- data.sh
+++ data.sh
@@ -3,5 +3,5 @@ sudo apt-get update
 sudo apt-get install -y apache2 wget
 sudo systemctl start apache2
 sudo systemctl enable apache2
-#sudo wget -O /tmp/index.html https://github.com/pepoangel/terrapri/blob/main/index.html
+https://github.com/pepoangel/terrapri/blob/main/data.sh
 #sudo mv /tmp/index.html /var/www/html/
