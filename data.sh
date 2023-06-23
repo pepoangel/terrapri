eb08a6b01edd843b9bd880db33676c6532ca3c71
Pepo <angeldavid@gmail.com>
2023-06-23 16:40:48 -0400

    wget

--- data.sh
+++ data.sh
@@ -3,5 +3,5 @@ sudo apt-get update
 sudo apt-get install -y niginx wget
 sudo systemctl start nginx
 sudo systemctl enable nginx
-#https://github.com/pepoangel/terrapri/blob/main/data.sh
+https://github.com/pepoangel/terrapri/blob/main/data.sh
 #sudo mv /tmp/index.html /var/www/html/
