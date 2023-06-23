7b405063483ea277d699a78496c72f0f3ae36935
Pepo <angeldavid@gmail.com>
2023-06-23 18:22:07 -0400

    ngnix

--- data.sh
+++ data.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 sudo apt update
-sudo apt install -y nginx wget
+sudo apt install -y nginx
+sudo rm /var/www/html/index.nginx-debian.html
+sudo cp index.html /var/www/html/
 sudo systemctl start nginx
 sudo systemctl enable nginx
-wget https://github.com/pepoangel/terrapri/blob/main/data.sh
-#sudo mv /tmp/index.html /var/www/html/
