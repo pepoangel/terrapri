b1f04191e07a1075ac9c4af9da80887889fe4062
Angel David Melendez A <63766459+pepoangel@users.noreply.github.com>
2023-07-19 09:47:34 -0400

    Delete data.sh

--- data.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-sudo apt update
-sudo apt install -y nginx
-sudo rm /var/www/html/index.nginx-debian.html
-sudo cp index.html /var/www/html/
-sudo systemctl start nginx
-sudo systemctl enable nginx
