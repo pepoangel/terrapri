4eb518ad6d51ec71a09d92f75d80825615358755
Pepo <angeldavid@gmail.com>
2023-07-20 19:12:15 -0400

    curso

--- main.tf
+++ main.tf
@@ -37,7 +37,13 @@ resource "aws_security_group" "allow_ssh" {
   name        = "allow_ssh"
   description = "Allow SSH inbound traffic"
   vpc_id      = var.vpc_id
-
+  ingress {
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
  
 
   egress {
